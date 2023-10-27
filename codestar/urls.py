739ca0c3cf70041369b5bcd01cd09184f3e67787
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-10-27 12:00:29 +0000

    The Post Detail View - part 1

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls'),
+    path('', include('blog.urls'), name='blog-urls'),
 ]
